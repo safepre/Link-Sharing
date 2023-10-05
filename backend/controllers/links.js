0e09aeb884a7a70e80063e8aca2757dad9078fbf
safepre <safelgpreciado@gmail.com>
2023-10-05 12:54:38 -0700

    associations added

--- backend/controllers/links.js
+++ backend/controllers/links.js
@@ -1,14 +1,19 @@
 const router = require('express').Router()
 const { Link } = require('../models')
-
+const { User } = require('../models')
 router.get('/', async (req, res) => {
-  const links = await Link.findAll()
-  res.json(links)
+  const users = await User.findAll({
+    include: {
+      model: Link,
+    },
+  })
+  res.json(users)
 })
 
 router.post('/', async (req, res) => {
   try {
-    const link = await Link.create(req.body)
+    const user = await User.findOne()
+    const link = await Link.create({ ...req.body, userId: user.id })
     res.json(link)
   } catch (error) {
     return res.status(400).json({ error })
