c288c8517034b8b5dc4f78e1ea2ff2c8446226ca
Safelg Preciado <safelgpreciado@gmail.com>
2023-10-17 00:04:54 -0700

    deployed

--- backend/util/config.js
+++ backend/util/config.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
-const POSTGRES_URL = process.env.NODE_ENV === 'test' ? process.env.TEST_DATABASE_URL : process.env.DATABASE_URL
+const POSTGRES_URL =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL
 module.exports = {
   POSTGRES_URL,
   PORT: process.env.PORT || 3001,
