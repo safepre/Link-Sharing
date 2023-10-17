317be160f9b9d45e96c68082c8bf8b46d0db07c8
Safelg Preciado <safelgpreciado@gmail.com>
2023-10-16 18:06:27 -0700

    fixed controllers in image

--- backend/controllers/images.js
+++ backend/controllers/images.js
@@ -2,9 +2,10 @@ const router = require('express').Router()
 const { Image } = require('../models')
 const { tokenExtractor, userExtractor } = require('../util/middleware')
 const upload = require('../util/multer')
-router.get('/:id', userExtractor, tokenExtractor, async (req, res) => {
+
+router.get('/:id', tokenExtractor,userExtractor, async (req, res) => {
   try {
-    const userId = req.params.userId
+    const userId = req.params.id
     const images = await Image.findAll({ where: { userId } })
     res.status(200).json(images)
   } catch (error) {
@@ -14,6 +15,7 @@ router.get('/:id', userExtractor, tokenExtractor, async (req, res) => {
 
 router.post(
   '/upload',
+  tokenExtractor,
   userExtractor,
   upload.single('image'),
   async (req, res) => {
@@ -38,31 +40,29 @@ router.post(
   }
 )
 
-router.put(
-  '/:id',
-  userExtractor,
-  tokenExtractor,
-  upload.single('image', async (req, res) => {
-    try {
-      if (!req.file) {
-        return res.status(400).json({ error: 'No file uploaded' })
-      }
-      const { userId, image_url } = req.params
-      if (!existingImage) {
-        return res.status(404).json({ error: 'Image not found' })
-      }
-      existingImage.file_name = req.file.originalname
-      existingImage.content_type = req.file.mimetype
-      existingImage.image_date = req.file.buffer
-      existingImage.file_size = req.file.size
-      existingImage.image_url = req.body.image_url
-      existingImage.updated_at = new Date()
-
-      await existingImage.save()
-      res.status(200).json(existingImage)
-    } catch (error) {
-      res.status(500).json({ error: 'Error updating the image' })
+router.put('/:id', tokenExtractor, userExtractor, upload.single('image'), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: 'No file uploaded' });
     }
-  })
-)
+
+    const imageId = req.params.id; // Assuming you're getting the image ID from the URL
+    const existingImage = await Image.findByPk(imageId); // Fetch the existing image by ID
+
+    if (!existingImage) {
+      return res.status(404).json({ error: 'Image not found' });
+    }
+
+    existingImage.file_name = req.file.originalname;
+    existingImage.content_type = req.file.mimetype;
+    existingImage.image_data = req.file.buffer; // Corrected 'image_data' property
+    existingImage.file_size = req.file.size;
+    existingImage.updated_at = new Date();
+
+    await existingImage.save();
+    res.status(200).json(existingImage);
+  } catch (error) {
+    res.status(500).json({ error: 'Error updating the image' });
+  }
+})
 module.exports = router
