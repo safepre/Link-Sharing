c972a5be8e46dd433716f2f44796ffe2327f31c8
Safelg Preciado <safelgpreciado@gmail.com>
2023-11-03 16:47:49 -0700

    Added CORS and signup controller

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
 
 const { PORT } = require('./util/config')
@@ -8,12 +10,16 @@ const linksRouter = require('./controllers/links')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const imageRouter = require('./controllers/images')
+
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/links', linksRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
 app.use('/api/images', imageRouter)
+
 const start = async () => {
   await connectToDatabase()
   app.listen(PORT, () => {
