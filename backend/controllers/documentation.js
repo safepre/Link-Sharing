e839651a4c5f44426d529ae20c45536dc681fd74
Safelg Preciado <safelgpreciado@gmail.com>
2023-12-17 10:12:10 -0800

    Documentation better now

--- backend/controllers/documentation.js
+++ backend/controllers/documentation.js
@@ -6,31 +6,51 @@ const apiDocumentation = `
  * #### Create User
  * - **Endpoint:** \`/api/signup\`
  * - **Method:** \`POST\`
- * - **Parameters:**
+ * - **Request Parameters:**
  *   - email_address (email)
  *   - password (hashed)
- *
+ *   - confirmPassword (must be same exact password as above)
+ * 
  * #### User Login
  * - **Endpoint:** \`/api/login\`
  * - **Method:** \`POST\`
- * - **Parameters:**
+ * - **Request Parameters:**
  *   - email_address (email)
  *   - password
- * - **Note:** Generates a token stored in the session table to associate the user with created links.
+ * - **Note:** Generates a jsonwebtoken that you can use in order to POST/PUT/DELETE Resources using bearer authorization.
  *
  * #### User Details
  * - **Endpoint:** \`/api/users\`
  * - **Methods:**
- *   - \`GET '/'\` (all users, excluding password hash and including user links)
- *   - \`GET '/:id'\` (specific user by ID, excluding password hash and including user links)
+ *   - \`GET '/'\` (all users, excluding password hash and including profile)
+ *   - \`GET '/:id'\` (specific user by ID, excluding password hash and including profile)
  * - **Note:** Two endpoints for retrieving user details.
  *
  * #### Update User
  * - **Endpoint:** \`/api/users/:id\`
  * - **Method:** \`PUT\`
- * - **Parameters:**
- *   - Username (new email/username)
+ * - **Request Parameters:**
+ *   - email_address (if you want to change your email_address)
  *
+ * ### Profile Management
+ * #### Create Profile
+ * - **Endpoint:** \`/api/profiles\`
+ * - **Authorization bearer required**
+ * - **Method:** \`POST\`
+ * - **Request Parameters:**
+ *   - first_name (string): Name in the format Safelg (not safelg or sAfelg or an empty string)
+ *   - last_name (string): Name in the format Foo (not foo or an empty string)
+ *   - email (string): Must be a valid email address (not a random word without @)
+ *
+ * #### Update Profile
+ * - **Endpoint:** \`/api/profiles/:id\`
+ * - **Authorization bearer required**
+ * - **Method:** \`PUT\`
+ * - **Request Parameters:**
+ *   - first_name (string): Updated first name (optional)
+ *   - last_name (string): Updated last name (optional)
+ *   - email (string): Updated email address (optional)
+ *  
  * ### Link Management
  * #### Get User Links
  * - **Endpoint:** \`/api/links\`
@@ -40,67 +60,37 @@ const apiDocumentation = `
  *
  * #### Create Link
  * - **Endpoint:** \`/api/links\`
+ * - **Authorization bearer required**
  * - **Method:** \`POST\`
- * - **Parameters:**
- *   - Token (from user using bearer authorization)
+ * - **Request Parameters:**
  *   - url (must be a valid URL)
  *   - platform (Git, Replit, Codewars, etc)
  *   - Date (automatically generated)
  *
  * #### Delete Link
  * - **Endpoint:** \`/api/links/:id\`
- * - **Method:** \`DELETE\`
- * - **Parameters:**
- *   - Token (from the user)
- *   - Link ID (to delete)
+ * - **Authorization bearer required**
+ * - **Method:** \`DELETE\` 
  *
  * #### Update Link
  * - **Endpoint:** \`/api/links/:id\`
+ * - **Authorization bearer required**
  * - **Method:** \`PUT\`
- * - **Parameters:**
- *  - Token (from the user)
- *  - Link ID (To Update)
+ * - **Request Parameters:**
+ * - url (must be a valid URL)
+ * - platform (Git, Replit, Codewars, etc)
  *  
- * ### Profile Management
- * #### Create Profile
- * - **Endpoint:** \`/api/profiles\`
- * - **Method:** \`POST\`
- * - **Parameters:**
- *   - Token (from user)
- *   - first_name (string): Name in the format Safelg (not safelg or sAfelg or an empty string)
- *   - last_name (string): Name in the format Foo (not foo or an empty string)
- *   - email (string): Must be a valid email address (not a random word without @)
- *
- * #### Update Profile
- * - **Endpoint:** \`/api/profiles/:id\`
- * - **Method:** \`PUT\`
- * - **Parameters:**
- *   - Token (from user)
- *   - first_name (string): Updated first name (optional)
- *   - last_name (string): Updated last name (optional)
- *   - email (string): Updated email address (optional)
- * 
  * ### Image Handling
+ * Make sure that Content-Type is multipart/form-data and body is form-data as well
  * #### Upload Photo
  * - **Endpoint:** \`/api/images/upload/\`
+ * - **Authorization bearer required**
  * - **Method:** \`POST\`
- * - **Parameters:**
- *   - file_name (string): Original name of the uploaded file
- *   - content_type (string): MIME type of the file
- *   - image_data (buffer): Binary data of the image file
- *   - file_size (number): Size of the file in bytes
- *   - created_at (date): Date and time when the file was uploaded
- *   - updated_at (date): Date and time when the file was last updated
  * 
  * #### Update Photo (cannot be removed, refer to Figma design)
- * - **Endpoint:** \`/api/images/upload\`
+ * - **Endpoint:** \`/api/images/upload/\`
+ * - **Authorization bearer required**
  * - **Method:** \`PUT\`
- * - **Parameters:**
- *   - file_name (string): Original name of the uploaded file
- *   - content_type (string): MIME type of the file
- *   - image_data (buffer): Binary data of the image file
- *   - file_size (number): Size of the file in bytes
- *   - updated_at (date): Date and time when the file was last updated
 `
 
 // Convert markdown to HTML
