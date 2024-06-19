425bb09878e67023ace482789d14225932452d12
bobasafe <safelgpreciado@gmail.com>
2024-06-19 16:58:30 -0700

    feat: add profile details content along w/ buttons on both tabs

--- linksharing/app/home/layout.tsx
+++ linksharing/app/home/layout.tsx
@@ -2,9 +2,10 @@
 
 import { NavbarComponent } from '@/components/NavBar'
 import CreateButton from '../../components/CreateButton'
-import Link from 'next/link'
 import LinkContent from '../../components/LinkContent'
 import { useState } from 'react'
+import { customThemeInput } from '../../utils/helperTheme'
+import { Button, TextInput } from 'flowbite-react'
 
 const HomeLayout = ({ children }) => {
   const [selectedButton, setSelectedButton] = useState('profile')
@@ -22,7 +23,7 @@ const HomeLayout = ({ children }) => {
         <div className="mt-3 flex justify-center gap-6 px-6 ">
           <div className="bg-white w-[1000px] h-[725px]">s</div>
           {selectedButton === 'profile' ? (
-            <div className="relative bg-white w-full h-[725px]">
+            <div className="overflow-auto relative bg-white w-full h-[725px]">
               <div className="m-10">
                 <span className="font-semibold text-3xl">
                   Customize your links
@@ -33,20 +34,93 @@ const HomeLayout = ({ children }) => {
                     with the world!
                   </span>
                 </div>
+                <CreateButton />
+                <LinkContent remove={undefined} />
               </div>
-              <div className="border-t absolute bg-white inset-x-0 bottom-0 h-16"></div>
+              <div className="border-t absolute bg-white inset-x-0 bottom-0 h-16">
+                {' '}
+                <div className="flex justify-end items-center h-full mr-7">
+                  <Button theme={customThemeInput} color="purple">
+                    Save
+                  </Button>
+                </div>
+              </div>
             </div>
           ) : (
             <div className="relative bg-white w-full h-[725px]">
               <div className="m-10">
-                <span className="font-semibold text-3xl">
-                  Your Name is Cool
-                </span>
-                <div className="mt-1 mb-6">
-                  <span className="text-sm text-slate-500">Hello World</span>
+                <span className="font-semibold text-3xl">Profile Details</span>
+                <div className="mt-1 mb-2">
+                  <span className="text-sm text-slate-500">
+                    Add your details to create a personal touch to your profile.
+                  </span>
+                  <div className="flex flex-col mt-7 justify-center items-center">
+                    <div className="flex flex-col gap-4">
+                      <div className="bg-zinc-100 rounded-md p-4">
+                        <div className="grid grid-cols-3 items-center">
+                          <span className="text-slate-500">
+                            Profile picture
+                          </span>
+                          <button className="bg-purple-200 w-[194px] h-[193px] rounded flex justify-center items-center text-purple-700 font-medium">
+                            <span>+ Upload Image</span>
+                          </button>
+                          <span className="text-slate-500 text-sm">
+                            Image must be below 1024x1024px. Use PNG or JPG
+                            format.
+                          </span>
+                        </div>
+                      </div>
+
+                      <div className="bg-zinc-100 rounded-md w-[739px] h-[200px] bg-gray-300">
+                        {' '}
+                        <div className="flex flex-row justify-between mt-3.5">
+                          <span className="m-4"> First name*</span>
+                          <TextInput
+                            className="m-2 w-[432px] mr-5"
+                            theme={customThemeInput}
+                            color="white"
+                            id="url"
+                            type="url"
+                            placeholder="e.g. John"
+                            required
+                          />
+                        </div>
+                        <div className="flex flex-row justify-between">
+                          <span className="m-4"> Last name*</span>
+                          <TextInput
+                            className="m-2 w-[432px] mr-5"
+                            theme={customThemeInput}
+                            color="white"
+                            id="url"
+                            type="url"
+                            placeholder="e.g. John"
+                            required
+                          />
+                        </div>
+                        <div className="flex flex-row justify-between">
+                          <span className="m-4"> Email</span>
+                          <TextInput
+                            className="m-2 w-[432px] mr-5"
+                            theme={customThemeInput}
+                            color="white"
+                            id="url"
+                            type="url"
+                            placeholder="e.g. John"
+                            required
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div className="border-t absolute bg-white inset-x-0 bottom-0 h-16"></div>
+              <div className="border-t absolute bg-white inset-x-0 bottom-0 h-16">
+                <div className="flex justify-end items-center h-full mr-7">
+                  <Button theme={customThemeInput} color="purple">
+                    Save
+                  </Button>
+                </div>
+              </div>
             </div>
           )}
         </div>
@@ -57,3 +131,4 @@ const HomeLayout = ({ children }) => {
 }
 
 export default HomeLayout
+1
